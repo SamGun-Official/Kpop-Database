5a3316607ed6bb3baf6e054083f708781ef5b0b8
Samuel Gunawan <samgun9d32@gmail.com>
2023-05-18 10:57:14 +0700

    Add new endpoint for fetch first profile picture

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -36,4 +36,39 @@ module.exports = {
 			return res.status(error.request ? error.request.res.statusCode : 400).send({ message: error.original ? error.original : error.message });
 		}
 	},
+	getOnlyFirst: async function (req, res) {
+		try {
+			const artist_id = req.params.artist_id;
+			const listProfile = await db.Profile.findAll({
+				where: {
+					artist_id: {
+						[Op.eq]: artist_id,
+					},
+				},
+				order: [["id", "ASC"]],
+				raw: true,
+			});
+			if (listProfile.length <= 0) {
+				throw {
+					request: {
+						res: {
+							statusCode: 404,
+						},
+					},
+					original: "No profile was found!",
+					message: "No profile was found!",
+				};
+			}
+
+			listProfile.forEach((profile) => {
+				delete profile.created_at;
+				delete profile.updated_at;
+				delete profile.deleted_at;
+			});
+
+			return res.status(200).send(listProfile[0]);
+		} catch (error) {
+			return res.status(error.request ? error.request.res.statusCode : 400).send({ message: error.original ? error.original : error.message });
+		}
+	},
 };
--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -5,5 +5,6 @@ const controller = require("../controllers/profile.controller");
 
 /* ===== ROUTES ===== */
 router.get("/:artist_id", controller.getByArtist);
+router.get("/fetch/:artist_id", controller.getOnlyFirst);
 
 module.exports = router;
